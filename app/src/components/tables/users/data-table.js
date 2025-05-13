98fb562b4fad03964257e9e6165506a1e9a8dc2a
Tepah <67202848+Tepah@users.noreply.github.com>
2025-05-12 19:23:19 -0700

    Made Button to show inactive users and inactive

--- app/src/components/tables/users/data-table.js
+++ app/src/components/tables/users/data-table.js
@@ -21,31 +21,36 @@ import {
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
 import { Input } from "@/components/ui/input"
- 
+import Link from "next/link";
 
-export function  DataTable({children, columns, data}) {
+
+export function DataTable({ children, columns, data }) {
   const [sorting, setSorting] = useState([]);
   const [columnFilters, setColumnFilters] = useState([]);
   const [activeRole, setActiveRole] = useState(null);
+  const [activeStatus, setActiveStatus] = useState("Active");
+  const [columnVisibility, setColumnVisibility] = useState({status: false});
 
   const table = useReactTable({
-      data,
-      columns,
-      getCoreRowModel: getCoreRowModel(),
-      getPaginationRowModel: getPaginationRowModel(),
-      onSortingChange: setSorting,
-      onColumnFiltersChange: setColumnFilters,
-      getFilteredRowModel: getFilteredRowModel(),
-      getSortedRowModel: getSortedRowModel(),
-      state: {
-        sorting,
-        columnFilters,
-      },
-    });
-  
-    return (
-      <div>
-        <div className="flex items-center py-4 space-x-2">
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility
+    },
+  });
+
+  return (
+    <div>
+      <div className="flex items-center py-4 space-x-2">
+        <div className="flex-1 flex flex-row justify-between">
           <Input
             placeholder="Filter emails..."
             value={(table.getColumn("email")?.getFilterValue()) ?? ""}
@@ -54,9 +59,9 @@ export function  DataTable({children, columns, data}) {
             }
             className="max-w-sm"
           />
-          <div className="flex items-center justify-end space-x-2 py-4">
+          <div className="flex items-center space-x-2">
             <span>Roles:</span>
-            <Button 
+            <Button
               variant={activeRole === "Admin" ? "default" : "outline"}
               size="sm"
               value="Admin"
@@ -79,7 +84,7 @@ export function  DataTable({children, columns, data}) {
             >
               Admin
             </Button>
-            <Button 
+            <Button
               variant={activeRole === "Student" ? "default" : "outline"}
               value="Student"
               size="sm"
@@ -98,7 +103,7 @@ export function  DataTable({children, columns, data}) {
                   { id: "role", value: selectedRole },
                 ]);
               }}
-              >
+            >
               Student
             </Button>
             <Button
@@ -120,59 +125,83 @@ export function  DataTable({children, columns, data}) {
                   { id: "role", value: selectedRole },
                 ]);
               }}
-              >
+            >
               Teacher
             </Button>
           </div>
         </div>
+      </div>
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => {
-                    return (
-                      <TableHead key={header.id}>
-                        {header.isPlaceholder
-                          ? null
-                          : (
-                            <div className="flex items-center justify-between">
-                              {flexRender(
-                                header.column.columnDef.header,
-                                header.getContext()
-                              )}
-                            </div>)}
-                      </TableHead>
-                    );
-                  })}
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : (
+                          <div className="flex items-center justify-between">
+                            {flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                          </div>)}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
                 </TableRow>
-              ))}
-            </TableHeader>
-            <TableBody>
-              {table.getRowModel().rows?.length ? (
-                table.getRowModel().rows.map((row) => (
-                  <TableRow
-                    key={row.id}
-                  >
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id}>
-                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell colSpan={columns.length} className="h-24 text-center">
-                    No results.
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </div>
-        <div className="flex items-center justify-end space-x-2 py-4">
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-between space-x-2 py-4">
+        <Button 
+          variant="default"
+          size="sm"
+          onClick={() => {
+            if (activeStatus === "Active") {
+              setActiveStatus("Inactive");
+              table.setColumnFilters((old) =>
+                old.filter((f) => f.id !== "status")
+              );
+            } else {
+              setActiveStatus("Active");
+              table.setColumnFilters((old) => [
+                ...old,
+                { id: "status", value: activeStatus },
+              ]);
+            }
+          }}
+          >
+            {activeStatus !== "Active" ? "See Active Users" : "See Inactive Users"}
+        </Button>
+
+
+        <div className="flex items-center space-x-2">
           {children}
           <span className="text-sm">
             Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
@@ -193,7 +222,8 @@ export function  DataTable({children, columns, data}) {
           >
             Next
           </Button>
+        </div>
+      </div>
     </div>
-  </div>
   );
 }
--- app/src/pages/admin/users.js
+++ app/src/pages/admin/users.js
@@ -49,7 +49,7 @@ export default function Users() {
 
   return (
     <Layout>
-      <div className="container w-[800px] p-12">
+      <div className="container mx-auto flex flex-col flex-1 max-w-[900px] p-8">
       <Label>
         <h1 className="text-3xl font-bold">Users</h1>
         <p className="text-gray-500">Manage users in the system</p>
